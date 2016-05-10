909887bea22247a5f3256200b1e3ae9573326a99
plneappl <wegsim@sl.supra-net.net>
2016-05-10 20:12:05 +0200

    Whoops, didn't compute all linear combinations, only those of pairs bc I misunderstood the hint. Now it should be better. Result is still the same, but I updated it nonetheless to be safe.

--- ex2/ex2.10.hs
+++ ex2/ex2.10.hs
@@ -5,17 +5,17 @@ import Data.List
 type Z10_2 = [Z2]
 data Z2 = Zero | One deriving(Show, Enum, Bounded, Eq)
 instance Num Z2 where
-  Zero + Zero = Zero
-  One + One = Zero
-  _ + _ = One
-  One * One = One
-  _ * _ = Zero
-  abs x = x
-  signum Zero = Zero
-  signum One = One
+  Zero + Zero   = Zero
+  One  + One    = Zero
+  _    + _      = One
+  One  * One    = One
+  _    * _      = Zero
+  abs x         = x
+  signum Zero   = Zero
+  signum One    = One
   fromInteger 0 = Zero
   fromInteger _ = One
-  negate x = x
+  negate x      = x
 
 toInt :: Z2 -> Int
 toInt Zero = 0
@@ -25,9 +25,9 @@ r :: StdGen -> [Z2]
 r sg = map fromInteger (randomRs (0, 1) sg)
 
 vecs :: [Z2] -> [Z10_2]
-vecs zs = do
-  let v1 = take 10 zs
-  let zs' = drop 10 zs
+vecs zs = 
+  let v1  = take 10 zs
+      zs' = drop 10 zs in
   v1 : vecs zs'
 
 rVecs :: StdGen -> [Z10_2]
@@ -45,22 +45,29 @@ combinations 0 _  = [ [] ]
 combinations n xs = [ y:ys | y:xs' <- tails xs
                            , ys <- combinations (n-1) xs']
 
-linearcombination :: Z10_2 -> Z10_2 -> Z10_2
-linearcombination = zipWith (+)
+subsets :: [a] -> [[a]]
+subsets xs = concatMap (`combinations` xs) [0..(length xs)]
 
-containsDuplicates :: Eq a => [a] -> Bool
-containsDuplicates as = all (<= 1) $ map length $ group as
+subsets' :: [a] -> [[a]]
+subsets' xs = filter (\ ys -> length ys >= 2) $ subsets xs
+
+linearcombination :: [Z10_2] -> Z10_2
+linearcombination = foldr (zipWith (+)) (take 10 zeros)
+
+containsNoDuplicates :: Eq a => [a] -> Bool
+containsNoDuplicates as = all (<= 1) $ map length $ group as
 
 isVectorSpace :: [Z10_2] -> Bool
 isVectorSpace vs = 
-  let pairs = map tuplify2 (combinations 2 vs) 
-      lcs   = map (uncurry linearcombination) pairs in
-  containsDuplicates lcs
+  let subs = subsets vs
+      lcs  = map linearcombination subs in
+  -- check for 1*v1+1*v2      check for v1 == v2   check for (0, ..., 0)
+  containsNoDuplicates lcs
 
 vectorSpaces :: [Z10_2] -> [[Z10_2]]
 vectorSpaces vs = 
-  let tvs = take 4 vs
-      vs' = drop 4 vs
+  let tvs  = take 4 vs
+      vs'  = drop 4 vs
       tvs' = [tvs | isVectorSpace tvs] in
       tvs' ++ vectorSpaces vs'
   
@@ -76,17 +83,16 @@ dist v1 v2 = sum $ map toInt $ zipWith (+) v1 v2
 minDist :: [Z10_2] -> Int
 minDist vs = minimum $ map (uncurry dist . tuplify2) $ combinations 2 vs
 
--- If this crashes with 'Prelude.minimum: empty list', just run it again
 main :: IO()
 main = do
   rgen <- getStdGen
   let rVecs' = rVecs rgen
-  let tries = 10000
-  let vs = take tries $ vectorSpaces rVecs'
-  let dists = map (\ vs' -> (vs', minDist vs')) vs
+  let tries  = 10000
+  let vs     = take tries $ vectorSpaces rVecs'
+  let dists  = map (\ vs' -> (vs', minDist vs')) vs
   let dists' = map snd dists
-  let avg = fromIntegral (sum dists') / fromIntegral (length dists')
-  let best = maximumBy (\ (vs1, d1) (vs2, d2) -> compare d1 d2) dists
+  let avg    = fromIntegral (sum dists') / fromIntegral (length dists')
+  let best   = maximumBy (\ (vs1, d1) (vs2, d2) -> compare d1 d2) dists
   print $ "Average distance: " ++ show avg
   print $ "Best code: " ++ show best
 
